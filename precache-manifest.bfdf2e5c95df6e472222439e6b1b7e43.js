e7e2bad31165142e5af1ac6ab12ca76756614028
rezy3312 <45492208+rezy12@users.noreply.github.com>
2024-03-15 10:34:46 -0500

    Updates

---
+++ precache-manifest.bfdf2e5c95df6e472222439e6b1b7e43.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f982249699a86c5756bd8b2b7dc23073",
+    "url": "/Spicy-Cupz/index.html"
+  },
+  {
+    "revision": "51d89177ba178d9bb328",
+    "url": "/Spicy-Cupz/static/css/2.1895afd8.chunk.css"
+  },
+  {
+    "revision": "10073b835d88343e5444",
+    "url": "/Spicy-Cupz/static/css/main.b6055710.chunk.css"
+  },
+  {
+    "revision": "51d89177ba178d9bb328",
+    "url": "/Spicy-Cupz/static/js/2.abb64d76.chunk.js"
+  },
+  {
+    "revision": "10073b835d88343e5444",
+    "url": "/Spicy-Cupz/static/js/main.ef03cdc7.chunk.js"
+  },
+  {
+    "revision": "a86165e4523a591fab21",
+    "url": "/Spicy-Cupz/static/js/runtime~main.b7d9490e.js"
+  },
+  {
+    "revision": "0f9b3500c4b42ec8000cb55fc646d81a",
+    "url": "/Spicy-Cupz/static/media/flowers.0f9b3500.jpeg"
+  },
+  {
+    "revision": "536b1fd5f9ad911cd9d5935c20bd1916",
+    "url": "/Spicy-Cupz/static/media/img1.536b1fd5.png"
+  },
+  {
+    "revision": "ebde206b6202271932d4b70ca928dba7",
+    "url": "/Spicy-Cupz/static/media/img2.ebde206b.png"
+  },
+  {
+    "revision": "2947280981c11d57e3f65d2c4d44067f",
+    "url": "/Spicy-Cupz/static/media/img3.29472809.png"
+  },
+  {
+    "revision": "a55cf7a7db213e53bf87aa63fb9632a5",
+    "url": "/Spicy-Cupz/static/media/logo.a55cf7a7.png"
+  },
+  {
+    "revision": "fed41b6931614dcca880c36d819893d6",
+    "url": "/Spicy-Cupz/static/media/midcup.fed41b69.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Spicy-Cupz/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Spicy-Cupz/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Spicy-Cupz/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Spicy-Cupz/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cedac4b895c20e871e3d88dfd519520f",
+    "url": "/Spicy-Cupz/static/media/smile.cedac4b8.PNG"
+  },
+  {
+    "revision": "b8d8fcc59da709ee297f29e138d103d7",
+    "url": "/Spicy-Cupz/static/media/spiceballs.b8d8fcc5.png"
+  },
+  {
+    "revision": "c06db859ec5e3c107957ff4dae09c89f",
+    "url": "/Spicy-Cupz/static/media/stack-cup.c06db859.png"
+  },
+  {
+    "revision": "344783d00d64383740c721ab02995397",
+    "url": "/Spicy-Cupz/static/media/two-cup.344783d0.jpg"
+  }
+]);
